80489aba5d55b112ba19534561213b26092f930a
makaeenm <bachduyatk300@gmail.com>
2023-05-25 23:37:39 +0700

    Trying connect sql T_T

--- Noisql/Noisql.cs
+++ Noisql/Noisql.cs
@@ -6,14 +6,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
 namespace Noisql
 {
-    SqlDataAdapter dataAdapter; // truy xuất dữ liệu vào bảng
-    SqlCommand sqlCommand; // dùng để truy vấn và cập nhật CSDL
-    internal class Noisql
+    public class thuVien
     {
+        SqlDataAdapter dataAdapter; // truy xuất dữ liệu vào bảng
+        SqlCommand sqlCommand; // dùng để truy vấn và cập nhật CSDL
 
+        public thuVien()
+        {
+        }
+
+        public DataTable getAllSINHVIEN()
+        {
+            DataTable datatable = new DataTable();
+            string query = "select * from SINHVIEN";
+            using (SqlConnection sqlConnection = connection.getconnection())
+            {
+                sqlConnection.Open();
+                dataAdapter = new SqlDataAdapter(query, sqlConnection);
+                dataAdapter.Fill(datatable);
+                sqlConnection.Close();
+            }
+            return datatable;
+        }
     }
 }
