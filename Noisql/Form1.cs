a40b33380277c88e2bcce40420dd6a8a8e3cf6da
makaeenm <bachduyatk300@gmail.com>
2023-05-25 23:21:30 +0700

    Dang try connect sql

---
+++ Noisql/Form1.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Windows.Forms;
+
+
+namespace Noisql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Noisql/Noisql.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Noisql
+{
+    SqlDataAdapter dataAdapter; // truy xuất dữ liệu vào bảng
+    SqlCommand sqlCommand; // dùng để truy vấn và cập nhật CSDL
+    internal class Noisql
+    {
+
+    }
+}
