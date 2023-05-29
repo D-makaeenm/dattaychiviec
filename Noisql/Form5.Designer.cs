aedc9cad33c8b31389bdf45275c3084349304c63
makaeenm <bachduyatk300@gmail.com>
2023-05-29 22:25:20 +0700

    đã xong 2/5 bảng

--- Noisql/Form5.Designer.cs
+++ Noisql/Form5.Designer.cs
@@ -28,12 +28,291 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form5";
+            label11 = new Label();
+            textBox11 = new TextBox();
+            button5 = new Button();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            groupBox1 = new GroupBox();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(294, 11);
+            label11.Name = "label11";
+            label11.Size = new Size(136, 20);
+            label11.TabIndex = 26;
+            label11.Text = "Mã hàng muốn sửa";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(436, 8);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(150, 27);
+            textBox11.TabIndex = 25;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Lime;
+            button5.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(610, 155);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 36);
+            button5.TabIndex = 23;
+            button5.Text = "Refresh";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(436, 127);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(150, 27);
+            textBox8.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(436, 94);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(150, 27);
+            textBox7.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(436, 60);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(150, 27);
+            textBox6.TabIndex = 18;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(316, 64);
+            label8.Name = "label8";
+            label8.Size = new Size(106, 20);
+            label8.TabIndex = 15;
+            label8.Text = "Số lượng nhập";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(316, 97);
+            label7.Name = "label7";
+            label7.Size = new Size(62, 20);
+            label7.TabIndex = 14;
+            label7.Text = "Đơn giá";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(316, 130);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 20);
+            label6.TabIndex = 13;
+            label6.Text = "Đơn vị tính";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(16, 160);
+            label4.Name = "label4";
+            label4.Size = new Size(122, 20);
+            label4.TabIndex = 11;
+            label4.Text = "Mã nhà cung cấp";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(16, 127);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 20);
+            label3.TabIndex = 10;
+            label3.Text = "Ngày nhập";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(16, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Mã sản phẩm";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(16, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(122, 20);
+            label1.TabIndex = 8;
+            label1.Text = "Số hóa đơn nhập";
+            label1.Click += label1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(144, 127);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 27);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(144, 94);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(144, 60);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(610, 113);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 36);
+            button3.TabIndex = 2;
+            button3.Text = "Xóa";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Yellow;
+            button2.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(610, 71);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 36);
+            button2.TabIndex = 1;
+            button2.Text = "Sửa";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.SkyBlue;
+            button1.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(610, 29);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 36);
+            button1.TabIndex = 0;
+            button1.Text = "Thêm";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(33, 276);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(731, 258);
+            dataGridView1.TabIndex = 5;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.BackColor = Color.SpringGreen;
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(textBox11);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(33, 30);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(731, 224);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thêm thông tin";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(144, 160);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(150, 27);
+            textBox4.TabIndex = 6;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gainsboro;
+            ClientSize = new Size(800, 558);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox1);
+            Name = "Form5";
+            Text = "Form5";
+            Load += Form5_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+        private Label label11;
+        private TextBox textBox11;
+        private Button button5;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private GroupBox groupBox1;
+        private TextBox textBox4;
     }
 }
--- Noisql/Form5.cs
+++ Noisql/Form5.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Noisql
 {
@@ -15,6 +17,130 @@ namespace Noisql
         public Form5()
         {
             InitializeComponent();
+        }
+        string chuoiketnoi = @"Data Source=DuyVPro;Initial Catalog=BTL;Integrated Security=True;Trust Server Certificate=True";
+        string sql;
+        SqlConnection ketnoi;
+        SqlCommand thuchien;
+        SqlDataReader docdulieu;
+        int i = 0;
+        DataTable dt;
+        String ma;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sohdn = textBox1.Text;
+            string masp = textBox2.Text;
+            string ngaynhap = textBox3.Text;
+            string mancc = textBox4.Text;
+            string slnhap = textBox6.Text;
+            string dongia = textBox7.Text;
+            string dvt = textBox8.Text;
+            string vitri = textBox11.Text;
+            ketnoi.Open();
+            sql = @"update HDNhap
+            set 
+            [Số HĐN] = N'" + sohdn + "' ,[Mã SP] =  N'" + masp + "' ,[Ngày nhập]= N'" + ngaynhap + "'  ,[Địa chỉ]= N'" + mancc + "'  ,[Điện thoại]=N'" + slnhap + "' ,[Tên người bán] = N'" + dongia + "', [Mã số thuế]= N'" + dvt + "'" + $" where [Mã khách hàng] = N'" + vitri + " '";
+            MessageBox.Show("Đã sửa thành công !!");
+            textBox11.Clear();
+            thuchien = new SqlCommand(sql, ketnoi);
+            thuchien.ExecuteNonQuery();
+            xoa();
+            ketnoi.Close();
+            ketnoi.Open();
+            sql = "select* from HDNhap";
+            hienthi();
+            dataGridView1.DataSource = dt;
+            ketnoi.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sohdn = textBox1.Text;
+            string masp = textBox2.Text;
+            string ngaynhap = textBox3.Text;
+            string mancc = textBox4.Text;
+            string slnhap = textBox6.Text;
+            string dongia = textBox7.Text;
+            string dvt = textBox8.Text;
+            string vitri = textBox11.Text;
+            ketnoi.Open();
+            sql = @"insert into HDNhap values
+            (N'" + sohdn + "', N'" + masp + "', N'" + ngaynhap + "', N'" + mancc + "',N'" + slnhap + "',N'" + dongia + "',N'" + dvt + "',N'" + vitri + "')";
+            MessageBox.Show("Thêm thành công!!!");
+            thuchien = new SqlCommand(sql, ketnoi);
+            thuchien.ExecuteNonQuery();
+            xoa();
+            ketnoi.Close();
+            ketnoi.Open();
+            sql = "select* from HDNhap";
+            hienthi();
+            dataGridView1.DataSource = dt;
+            ketnoi.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ketnoi.Open();
+            string lenhxoa = "delete HDNhap where [Số HĐN] ='" + ma + "'";
+
+            thuchien = new SqlCommand(lenhxoa, ketnoi);
+            thuchien.ExecuteNonQuery();
+            ketnoi.Close();
+
+            ketnoi.Open();
+            sql = "select* from HDNhap";
+            hienthi();
+            dataGridView1.DataSource = dt;
+            ketnoi.Close();
+        }
+        public void hienthi()
+        {
+            thuchien = new SqlCommand(sql, ketnoi);
+            docdulieu = thuchien.ExecuteReader();
+            dt = new DataTable();
+            dt.Load(docdulieu);
+        }
+        public void xoa()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            xoa();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            ketnoi = new SqlConnection(chuoiketnoi);
+            ketnoi.Open();
+            sql = "select* from HDNhap";
+            hienthi();
+            dataGridView1.DataSource = dt;
+            ketnoi.Close();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex == -1) { return; }
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+            ma = row.Cells[0].Value.ToString();
         }
     }
 }
